2152fbbf37b0d45b777b72c92813ad351b57a001
shaogeng1994(COM-PC) <shaogeng1994@outlook.com>
2017-03-16 17:37:12 +0800

    独立FileProvider

---
+++ JsjhBase/baselibrary/src/main/java/com/jsojs/baselibrary/util/UpdateFileProvider.java
@@ -0,0 +1,10 @@
+package com.jsojs.baselibrary.util;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Administrator on 2017/3/16.
+ */
+
+public class UpdateFileProvider extends FileProvider {
+}
